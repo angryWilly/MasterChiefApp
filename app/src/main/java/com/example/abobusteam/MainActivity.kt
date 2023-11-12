49626dafe623dc033d148abed731e5d60c67d707
angryWilly <kirill.teyxrib@yandex.ru>
2023-11-12 17:40:27 +0700

    kirill init

--- app/src/main/java/com/example/abobusteam/MainActivity.kt
+++ app/src/main/java/com/example/abobusteam/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-            text = "Hello, ABOBUSTEAM $name!",
+            text = "Hello, Kirill $name!",
             modifier = modifier
     )
 }
