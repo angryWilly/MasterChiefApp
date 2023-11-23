071dba8f5a3ada7a3a0e4d3c15fa7cabbfe4932a
angryWilly <kirill.teyxrib@yandex.ru>
2023-11-24 01:17:18 +0700

    fix merge issues

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,7 +67,5 @@ dependencies {
 
     implementation ("androidx.navigation:navigation-compose:2.7.5")
     implementation ("androidx.compose.material:material:1.5.4")
-
-    implementation("com.github.skydoves:landscapist-glide:2.2.12")
-
+    implementation("io.coil-kt:coil-compose:2.5.0")
 }
